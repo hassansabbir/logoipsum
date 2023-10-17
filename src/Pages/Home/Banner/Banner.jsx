d68e32d400ac863dc1e6682b6b868bbfa1bdb312
hassansabbir <mahmoodsabbir3087@gmail.com>
2023-10-18 00:43:24 +0600

    elu

---
+++ src/Pages/Home/Banner/Banner.jsx
@@ -0,0 +1,28 @@
+import image1 from "../../../assets/banner1.png";
+import image2 from "../../../assets/banner2.png";
+import image3 from "../../../assets/banner3.png";
+import image4 from "../../../assets/bannerBottom.png";
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div className="bannerSection flex items-end gap-10">
+      <div>
+        <h2 className="text-5xl font-bold ">
+          Find{" "}
+          <span className="bg-clip-text text-transparent bg-gradient-to-r from-blue-600 to-purple-600">
+            Partners
+          </span>{" "}
+          ( CAs ) <br /> available online
+        </h2>
+      </div>
+      <div className="flex gap-10">
+        <img src={image1} alt="" />
+        <img src={image2} alt="" />
+        <img src={image3} alt="" />
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -47,7 +47,7 @@ const Navbar = () => {
   );
 
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar fixed max-w-7xl">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
